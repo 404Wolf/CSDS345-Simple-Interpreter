36e63815f3bd42bf471bcc4cda748501b31acb60
Wolf Mermelstein <wolf@404wolf.com>
2025-02-24 21:22:37 -0500

    add M_value

--- src/interpreter.rkt
+++ src/interpreter.rkt
@@ -9,24 +9,38 @@
 ;; (interpret read-line)
 
 ; Get all of the variable names
-(define (var-names s)
-  (map car s))
+(define (all-var-names state)
+  (map car state))
 
 ; Get all of the variable values
-(define (var-values s)
-  (map cdr s))
+(define (all-var-values state)
+  (map cdr state))
 
 ; Get the value of a variable in the variable mappings
-(define (var-value s var)
-  (if (member var (var-names s))
-      (cadar (filter (lambda (v) (eq? (car v) var)) s))
+(define (get_var_value state var)
+  (if (member var (all-var-names state))
+      (cadar (filter (lambda (v) (eq? (car v) var)) state))
       #f))
 
+; Map an return statement
+(define (M_return expr breaker)
+  (breaker (M_value expr)))
+
+; Map a value (either an int, bool, or variable)
+(define (M_value value state)
+  (or (map (lambda (f) (f value state)) (list M_int M_bool))))
+
 ; Map an int expression into a number
-(define (M_int x s)
-  (if (number? x)
-      x
-      (var-value s x)))
+(define (M_int int state)
+  (if (number? int)
+      int
+      (get_var_value state int)))
+
+; Map an int expression into a number
+(define (M_bool bool state)
+  (if (number? bool)
+      bool
+      (get_var_value state bool)))
 
 ; Map an number operator into a function
 (define M_num_ops
