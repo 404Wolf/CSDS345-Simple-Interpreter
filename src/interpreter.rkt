732f6040e0ded9448a7bb93e36c04779e5d2365a
Wolf Mermelstein <wolf@404wolf.com>
2025-02-24 20:36:43 -0500

    add operators

--- src/interpreter.rkt
+++ src/interpreter.rkt
@@ -1,26 +1,52 @@
 #lang racket
 
 (require "simpleParser.rkt")
+(provide (all-defined-out))
 
-(define interpret (lambda (filename) (parser (filename))))
+(define (interpret filename)
+  (parser (filename)))
 
 ;; (interpret read-line)
 
 ; Get all of the variable names
-(define var-names (lambda (s) (map car s)))
+(define (var-names s)
+  (map car s))
 
 ; Get all of the variable values
-(define var-values (lambda (s) (map cdr s)))
+(define (var-values s)
+  (map cdr s))
 
 ; Get the value of a variable in the variable mappings
-(define var-value
-  (lambda (s var)
-    (if (member var (var-names s))
-        (cadar (filter (lambda (v) (eq? (car v) var)) s))
-        #f)))
+(define (var-value s var)
+  (if (member var (var-names s))
+      (cadar (filter (lambda (v) (eq? (car v) var)) s))
+      #f))
 
-(define M_int
-  (lambda (int state)
-    (cond
-      [(member state) (list-ref (index-of (state int)))]
-      [(number? int) int])))
+; Map an int expression into a number
+(define (M_int x s)
+  (if (number? x)
+      x
+      (var-value s x)))
+
+; Map an number operator into a function
+(define M_num_ops
+  (match-λ ['+ +] ;
+           ['- -]
+           ['* *]
+           ['/ /]
+           ['% modulo]))
+
+; Map an boolean operator into a function
+(define M_bool_ops
+  (match-λ ['&& (λ (a b) (and a b))] ;
+           ['|| (λ (a b) (or a b))]
+           ['! (λ (a) (not a))]))
+
+; Map an bool comparison operator into a function
+(define (M_comp_ops x)
+  (('== eq?) ;
+   ('!= (λ (a b) (not (eq? a b))))
+   ('< <)
+   ('> >)
+   ('<= <=)
+   ('>= >=)))
---
+++ src/tests.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require "interpreter.rkt")
+
+(define (assert condition message)
+  (if (not condition)
+      (begin
+        (display "Assertion failed: ")
+        (display message)
+        (newline)
+        #f)
+      #t))
+
+(define sample-state '((y 5) (b 2) (a 2)))
+
+; M_int tests
+(assert (eq? 5 (M_int 5 sample-state)) "result should be 5")
+(assert (eq? 2 (M_int 'b sample-state)) "result should be value b=2")
+
+; Test boolean AND (&&)
+(assert (eq? #t ((M_bool_ops '&&) #t #t)) "true AND true should be true")
+
+; Test boolean OR (||)
+(assert (eq? #t ((M_bool_ops '||) #t #t)) "true OR true should be true")
+
+; Test boolean NOT (!)
+(assert (eq? #f ((M_bool_ops '!) #t)) "NOT true should be false")
