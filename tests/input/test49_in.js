ecc44ba9d654f8fc61919f08ff8c8e5205568bbe
Wolf Mermelstein <wolf@404wolf.com>
2025-04-26 21:23:13 -0400

    Fix declaration location in tests

--- tests/input/test49_in.js
+++ tests/input/test49_in.js
@@ -1,14 +1,14 @@
 class A {
+  static function main() {
     function fib(a) {
-  if (a == 0)
-    return 0;
-  else if (a == 1)
-    return 1;
-  else 
-    return fib(a-1) + fib(a-2);
-}
+      if (a == 0)
+        return 0;
+      else if (a == 1)
+        return 1;
+      else 
+        return fib(a-1) + fib(a-2);
+    }
 
-static function main() {
-  return fib(10);
-}
+    return fib(10);
   }
+}
